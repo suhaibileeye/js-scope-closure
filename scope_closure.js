a5ce423e1aac6a488b40e4bda7eb014d95af9dc3
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2021-02-13 09:34:56 -0600

    Update scope_closure.js

--- scope_closure.js
+++ scope_closure.js
@@ -2,6 +2,21 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 
+
+// ==== Challenge 2: Flip A Coin ====
+ 
+   // Use the function below flip(), give it one parameter called "repeat" and make it so that every time we call it and give it a number, it flips a coin and gives us "Heads" or "Tails"
+   // Use Math.round(Math.random()) inside the function to generate a random number, which will be the coin flip
+   // Use for loop inside the function to repeat the process of flipping the coin
+   // always return "Heads" or "Tails" and log your result on the console.
+
+    function flip(repeat){
+        // Your code goes here
+    }
+
+
+
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
