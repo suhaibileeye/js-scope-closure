1b049be376b5f3725226c1361423354d53357388
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2021-02-13 22:09:56 -0600

    Update scope_closure.js

--- scope_closure.js
+++ scope_closure.js
@@ -1,18 +1,111 @@
-// ==== Challenge 1: Write your own closure ====
-// Write a simple closure of your own creation.  Keep it simple!
+///// M V P ///////
+
+/******************************************************************************\
+	Task 1: `counterMaker`
+\******************************************************************************/
+/****** INSTRUCTIONS TASK 1 ******/
+/* 
+ * Study the code below for counter1 and counter2. Answer the questions below.
+ * 
+ * 1. What is the difference between counter1 and counter2?
+ * 
+ * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ *
+*/
+
+// counter1 code
+function counterMaker() {
+  let count = 0;
+  return function counter() {
+    count++;
+  }
+}
+
+const counter1 = counterMaker();
+
+// counter2 code
+let count = 0;
+
+function counter2() {
+  return count++;
+}
 
 
+/******************************************************************************\
+	Task 2: Get Motivated
+\******************************************************************************/
 
-// ==== Challenge 2: Flip A Coin ====
- 
-   // Use the function below flip(), give it one parameter called "repeat" and make it so that every time we call it and give it a number, it flips a coin and gives us "Heads" or "Tails"
-   // Use Math.round(Math.random()) inside the function to generate a random number, which will be the coin flip
-   // Use for loop inside the function to repeat the process of flipping the coin
-   // always return "Heads" or "Tails" and log your result on the console.
+/****** INSTRUCTIONS TASK 2 ******/
+/* Inside the motivation function create another function called message that
+will return 'You're doing awesome, keep it up firstname lastname.' */
 
-    function flip(repeat){
-        // Your code goes here
-    }
+function motivation(firstname, lastname) {
+
+  var welcomeText = 'You\'re doing awesome, keep it up ';
+
+  // code message function here.
+
+
+  //Uncommment this to return the value of your invoked message function
+  //return message();
+
+}
+
+motivation('Mohamed', 'Ali'); // 'You're doing awesome keep it up Mohamed Ali.
+
+
+/******************************************************************************\
+ Task 3: Find Your Friends
+ \******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 7 ******/
+/* Here we are given three arrays: an array of friends, an array of second-level
+friends (friends of friends), and an array of all users. These arrays may share
+users. Write a function that takes in our existing friends and returns
+a function that will tell us if a given user is not already a friend. */
+
+var friends = ["Ahmed", "Khadijo", "Farah"];
+var secondLevelFriends = ["Mahad", "Farah", "Mohamed"];
+var allUsers = ["Ahmed", "Khadijo", "Farah", "Mahad", "Mohamed", "Bashir", "Ali"];
+
+function findPotentialFriends(existingFriends) {
+
+}
+
+var isNotAFriend = findPotentialFriends( friends );
+// isNotAFriend(allUsers[0]); // false
+// isNotAFriend(secondLevelFriends[2]); // true
+
+
+/******************************************************************************\
+	Task 4: Keep a log
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 8 ******/
+/* Here we have a for loop that will iterate as long as i is less than or equal
+to 5. What we need to do is console.log(i) so that it logs like so:
+ 0 second after call - log 0
+ 1 seconds after call - log 1
+ 2 seconds after call - log 2
+ 3 seconds after call - log 3
+ 4 seconds after call - log 4
+ 5 seconds after call - log 5
+ However, because each call to console.log occurs after the loop has finished,
+ the value of i has changed before the console.log executes. We'll need to use
+ a closure to preserve a reference to i at the time of execution.
+ Fix the code below to log the desired output.
+ */
+
+function timeOutCounter() {
+  for (var i = 0; i <= 5; i++) {
+    setTimeout(function() {
+    	console.log(i)
+	}, i * 1000)
+  }
+}
+timeOutCounter();
 
 
 
