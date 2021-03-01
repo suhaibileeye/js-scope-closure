1aa8966d885b7d35f15063951b6071f6e78d507d
suhaibileeye <suhaibileeye@gmail.com>
2021-03-01 19:53:23 +0300

    first commit

--- scope_closure.js
+++ scope_closure.js
@@ -1,5 +1,4 @@
 ///// M V P ///////
-
 /******************************************************************************\
 	Task 1: `counterMaker`
 \******************************************************************************/
@@ -14,7 +13,6 @@
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
 */
-
 // counter1 code
 function counterMaker() {
   let count = 0;
@@ -22,67 +20,54 @@ function counterMaker() {
     count++;
   }
 }
-
 const counter1 = counterMaker();
-
 // counter2 code
 let count = 0;
-
 function counter2() {
   return count++;
 }
-
-
+// counter 1 is closure 
+//counter 1 code is the closure , because of it we are able 
+//to put funsctions together 
+//counter code 2 is  a basic closure 
 /******************************************************************************\
 	Task 2: Get Motivated
 \******************************************************************************/
-
 /****** INSTRUCTIONS TASK 2 ******/
 /* Inside the motivation function create another function called message that
 will return 'You're doing awesome, keep it up firstname lastname.' */
-
 function motivation(firstname, lastname) {
-
-  var welcomeText = 'You\'re doing awesome, keep it up ';
-
-  // code message function here.
-
-
+  var welcomeText = 'You are doing awesome, keep it up '
+     function message() {
+      let greeting = welcomeText +  firstname + lastname 
+      return  greeting 
+    }
   //Uncommment this to return the value of your invoked message function
-  //return message();
-
+   return message();
 }
-
-motivation('Mohamed', 'Ali'); // 'You're doing awesome keep it up Mohamed Ali.
-
-
+console.log(motivation('Abdirahman' , 'Ali ileeye')); // 'You're doing awesome keep it up Abdirahman Ali Ileeye.
 /******************************************************************************\
  Task 3: Find Your Friends
  \******************************************************************************/
-
 /****** INSTRUCTIONS PROBLEM 3 ******/
 /* Here we are given three arrays: an array of friends, an array of second-level
 friends (friends of friends), and an array of all users. These arrays may share
 users. Write a function that takes in our existing friends and returns
 a function that will tell us if a given user is not already a friend. */
-
-var friends = ["Ahmed", "Khadijo", "Farah"];
-var secondLevelFriends = ["Mahad", "Farah", "Mohamed"];
-var allUsers = ["Ahmed", "Khadijo", "Farah", "Mahad", "Mohamed", "Bashir", "Ali"];
-
+var friends = ["Abdirahman", "Abdulhakem", "Joseph"];
+var secondLevelFriends = ["Ricki Zakarie", "Salah nay'nay", "Ahmed Kev"];
+var allUsers = ["Abdirahman", "Abdulhakem", "Joseph", "Hassan", "Leekaashe", "Sacad", "kader mouse"];
 function findPotentialFriends(existingFriends) {
-
 }
 
 var isNotAFriend = findPotentialFriends( friends );
+
 // isNotAFriend(allUsers[0]); // false
 // isNotAFriend(secondLevelFriends[2]); // true
-
 
 /******************************************************************************\
 	Task 4: Keep a log
 \******************************************************************************/
-
 /****** INSTRUCTIONS PROBLEM 4 ******/
 /* Here we have a for loop that will iterate as long as i is less than or equal
 to 5. What we need to do is console.log(i) so that it logs like so:
@@ -97,45 +82,33 @@ to 5. What we need to do is console.log(i) so that it logs like so:
  a closure to preserve a reference to i at the time of execution.
  Fix the code below to log the desired output.
  */
-
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for ( var i = 0; i <= 5; i++) {
     setTimeout(function() {
     	console.log(i)
 	}, i * 1000)
-  }
+  }    
 }
 timeOutCounter();
-
-
+function Counter() {
+  var counter = 0;
+    function IncreaseCounter() {
+    return counter += 1;
+  };
+   return IncreaseCounter;
+}
+let countNum = Counter();
+alert(countNum()); 
+alert(countNum()); 
+alert(countNum()); 
+alert(countNum()); 
 /******************************************************************************\
 	Task 5: Check if name exists
 \******************************************************************************/
-
 /****** INSTRUCTIONS PROBLEM 5 ******/
+
+
 /*
-  Write a function called contains that takes in three parameters: an array, a name and a callback.  
+ Write a function called contains that takes in three parameters: an array, a name and a callback.  
   Check if the name exists in the array. 
-  If it does, invoke the callback with true as the argument. 
-  If the name does not exist, invoke the callback with false as the argument.
-*/
-
-
-
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
-
-// ==== Challenge 2: Create a counter function ====
-const counter = () => {
-    // Return a function that when invoked increments and returns a counter variable.
-  };
-  // Example usage: const newCounter = counter();
-  // newCounter(); // 1
-  // newCounter(); // 2
-  
-  // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-  const counterFactory = () => {
-    // Return an object that has two methods called `increment` and `decrement`.
-    // `increment` should increment a counter variable in closure scope and return it.
-    // `decrement` should decrement the counter variable and return it.
-  };
+  If it does, invoke the callback with true as the argument.*/
